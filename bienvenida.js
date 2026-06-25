0a19024ecfa7e44d46d4c8fc0c65f4f115c90fca
yutencurso <yutencurso@gmail.com>
2026-06-25 17:51:16 +0200

    ygg

--- bienvenida.js
+++ bienvenida.js
@@ -1,14 +1,14 @@
-const usuario = localStorage.getItem('nombreDIDaccount');
-const frases = [
-    `A qué nos dedicaremos hoy?`,
-    `Hola, ${usuario}`,
-    `Que deseas hacer hoy?`,
-    `Son las productividad en punto, ${usuario}`,
-    `Modo ${usuario} activado`,
-    `Cuales son tus objetivos este día?`,
-    `Te damos la bienvenida, ${usuario}`
-]
 function fraseBienvenida() {
+    const usuario = localStorage.getItem('nombreDIDaccount') || 'invitado';
+    const frases = [
+        'A qué nos dedicaremos hoy?',
+        `Hola, ${usuario}`,
+        '¿Qué deseas hacer hoy?',
+        `Son las productividad en punto, ${usuario}`,
+        `Modo ${usuario} activado`,
+        '¿Cuáles son tus objetivos este día?',
+        `Te damos la bienvenida, ${usuario}`
+    ];
     const seleccionado = frases[Math.floor(Math.random() * frases.length)];
     return seleccionado;
 }
